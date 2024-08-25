e4150d430dd183dd9b82702baad7e203f9077f03
kth-tw <88102592+kth-tw@users.noreply.github.com>
2024-08-25 19:10:17 +0800

    init

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '63c6c253a38f5c64245598d1';
+_mrmcp['creative_id'] = '66cab5d431ac5952ab03860a';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 626;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '林政旭&簡語宸 婚禮邀請函';
+_mrmcp['build_number'] = parseInt('446');
+_mrmcp['publish_time'] = 'Sun Aug 25 2024 18:51:49 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
